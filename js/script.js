2e751b296b520336ecb6d14f6fe52406402268bc
Hugo Alvarez <ugovozz@hotmail.com>
2018-12-24 15:04:49 -0300

    Nuevas practias DOM

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Get access to the DOM
+const articles = document.getElementsByTagName('article');
+const paragraphs = document.getElementsByTagName('p');
+const firstArticle = articles[0];
+const secondParagraph = paragraphs[1];
+const whiteTextElements = document.getElementsByClassName('text-white');
+const sidebar = document.getElementById('sidebar');
+
+// Modify the DOM
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Modify the DOM';
+
+const header = document.getElementById('page-header');
+header.innerHTML = '<h2>Modify the DOM</h2>';
